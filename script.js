19e456285b880e00d48934dde86d3f936ac3a6cd
annacarmovieira-lab <anna.carmo.vieira@escola.pr.gov.br>
2026-05-29 08:11:01 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const botao = document.getElementById("botaoMensagem");
+
+botao.addEventListener("click", function() {
+    alert("O agronegócio sustentável une produtividade e preservação ambiental!");
+});
